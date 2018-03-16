6bc56139190da381e132547cf81af54886050426
2014004893 <prangia0ren@gmail.com>
2018-03-17 03:42:21 +0900

    interactive mode finished

--- proj_shell/include/shell.h
+++ proj_shell/include/shell.h
@@ -1,12 +1,35 @@
+#ifndef __SHELL_H__
+#define __SHELL_H__
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <ctype.h>
 
 //the maximum length of command line input
 #define MAX_CMDLINE 1024
 //the maximum parsable commands
 #define MAX_PARSED_CMD 100
+//the maximum number of arguments
+#define MAX_ARGUMENT 10
 
+struct sigaction act;
 const char* PS1 = "prompt > ";
 char cmdline_input[MAX_CMDLINE + 1];
 
+//Function will parse & trim the command line and use fork to process multiple
+//commands at a same time
 void execute_cmdline();
+//This is the actual function that will be used by child process to run each shell command
+void execute_cmd(char *cmd);
+//the original code was written by Adam Rosenfield
+//from https://stackoverflow.com/questions/122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
+size_t trim_whitespace(char *out, size_t len, const char *str);
+//capturing signal(SIGINT to be exact) means one of the parsed commands is "quit" command
+//parent process will wait till any other child process(rest of the commands) to finish it's work and terminate itself.
+static void sig_fn(int signo);
+
+#endif
--- proj_shell/src/shell.c
+++ proj_shell/src/shell.c
@@ -6,9 +6,10 @@ int main(int argc, char **argv)
 		while(1) {
 			printf("%s", PS1);
 			if (fgets(cmdline_input, MAX_CMDLINE, stdin) == NULL) {
+				//when nothing but "ctrl + D" is pressed, EOF will cause trouble with fgets.
 				return 0;
 			}
-			printf("%s", cmdline_input);
+			// printf("%s", cmdline_input);
 			execute_cmdline();
 		}
 	}
@@ -16,19 +17,135 @@ int main(int argc, char **argv)
 
 	}
 	else {
+		printf("Only one batchfile can be processed at a time.\n");
 		printf("\t\tUSAGE\n");
 		printf("Interactive mode: %s\n", argv[0]);
-		printf("Batch mode: %s [batchFile]\n", argv[0]);
+		printf("Batch mode: %s [batchfile]\n", argv[0]);
 	}
 }
 
 void execute_cmdline()
 {
+	pid_t pid;
 	char* parsed_cmd;
+	char trimmed_cmd[MAX_PARSED_CMD];
 
 	parsed_cmd = strtok(cmdline_input, ";");
-	while(parsed_cmd != NULL) {
-		printf("%s\n", parsed_cmd);
+	while (parsed_cmd != NULL) {
+		switch (pid = fork()) {
+			case -1: //error on fork()
+				fprintf(stderr, "failed to create child process\n");
+				fprintf(stderr, "%s\n", strerror(errno));
+				break;
+			case 0: //child process
+				if (strlen(parsed_cmd) > MAX_PARSED_CMD) {
+					fprintf(stderr, "cannot parse command longer than %d characters\n", MAX_PARSED_CMD);
+					exit(0);
+				}
+				trim_whitespace(trimmed_cmd, MAX_PARSED_CMD, parsed_cmd);
+				execute_cmd(trimmed_cmd);
+				exit(0);
+				break;
+			default: //parent process
+				//does nothing and continue to parsing commandline
+				break;
+		}
 		parsed_cmd = strtok(NULL, ";");
 	}
+	//wait for all child processes(commands) to be finished
+	while(wait(NULL) > 0);
+
+}
+
+void execute_cmd(char *cmd)
+{
+	// fprintf(stdout,"%s\n", cmd);
+	char *parsed_argv;
+	int argc = 0;
+	char **argv;
+
+	act.sa_handler = sig_fn;
+	sigfillset(&act.sa_mask);
+
+	argv = (char**)malloc(sizeof(char*) * MAX_ARGUMENT + 1);
+	for (int i = 0; i < MAX_ARGUMENT; ++i)
+		argv[i] = (char*)malloc(sizeof(char) * (MAX_PARSED_CMD + 1));
+
+	parsed_argv = strtok(cmd, " ");
+	while(parsed_argv != NULL) {
+		if (strlen(parsed_argv) > MAX_PARSED_CMD) {
+			fprintf(stderr, "each arguments for command cannot exceed %d.\n", MAX_PARSED_CMD);
+			for (int i = 0; i < MAX_ARGUMENT; ++i)
+				free(argv[i]);
+			free(argv);
+			return;
+		}
+		strncpy(argv[argc++], parsed_argv, strlen(parsed_argv));
+		if (argc == 1) {//check the command if it is "quit"
+			if (!strcmp(argv[0], "quit") || !strcmp(argv[0], "QUIT")) {
+				for (int i = 0; i < MAX_ARGUMENT; ++i)
+					free(argv[i]);
+				free(argv);
+				kill(getppid(), SIGINT);
+				return;
+			}
+		}
+		if (argc > MAX_ARGUMENT) {
+			fprintf(stderr, "number of arguments must be less than %d.\n", MAX_ARGUMENT);
+			for (int i = 0; i < MAX_ARGUMENT; ++i)
+				free(argv[i]);
+			free(argv);
+			return;
+		}
+		parsed_argv = strtok(NULL, " ");
+	}
+	for (int i = argc; i < MAX_ARGUMENT; ++i)
+		free(argv[i]);
+	argv[argc] = NULL;
+
+	if (execvp(argv[0], argv) == -1) {
+		fprintf(stderr, "failed to execute execvp\n");
+		for (int i = 0; i < argc; ++i)
+			free(argv[i]);
+		free(argv);
+		return;
+	}
+}
+
+size_t trim_whitespace(char *out, size_t len, const char *str)
+{
+	if(len == 0)
+		return 0;
+
+	const char *end;
+	size_t out_size;
+
+	// Trim leading space
+	while(isspace((unsigned char)*str)) str++;
+
+	if(*str == '\0')  // All spaces?
+	{
+		*out = '\0';
+		return 1;
+	}
+
+	// Trim trailing space
+	end = str + strlen(str) - 1;
+	while(end > str && isspace((unsigned char)*end)) end--;
+	end++;
+
+	// Set output size to minimum of trimmed string length and buffer size minus 1
+	out_size = (end - str) < len-1 ? (end - str) : len-1;
+
+	// Copy trimmed string and add null terminator
+	memcpy(out, str, out_size);
+	out[out_size] = '\0';
+
+	return out_size;
+}
+
+static void sig_fn(int signo)
+{
+	while(wait(NULL) > 0);
+	exit(0);
 }
