7cee49a92af6fafb70375e3ab051372965ec5810
DaeIn Lee <prangia0ren@gmail.com>
2018-03-21 19:15:02 +0900

    lab03

--- xv6-public/prac_syscall.c
+++ xv6-public/prac_syscall.c
@@ -8,3 +8,12 @@ printk_str(char *str)
 	cprintf("%s\n", str);
 	return 0xABCDABCD;
 }
+int
+sys_myfunction(void)
+{
+	char *str;
+	//Decode argument using argstr
+	if (argstr(0, &str) < 0)
+		return -1;
+	return printk_str(str);
+}
