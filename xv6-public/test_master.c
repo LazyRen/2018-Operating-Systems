2daa1efc2ada4893645552628d1341f85ebb8fbb
DaeIn Lee <prangia0ren@gmail.com>
2018-04-22 00:13:59 +0900

    test_master

--- xv6-public/test_master.c
+++ xv6-public/test_master.c
@@ -6,6 +6,7 @@
 #include "stat.h"
 #include "user.h"
 
+#define CNT_TEST					6
 // Number of child programs
 #define CNT_CHILD           15
 #define CNT_EXCEED          5
@@ -14,8 +15,13 @@
 #define NAME_CHILD_STRIDE   "test_stride"
 // Name of child test program that tests MLFQ scheduler
 #define NAME_CHILD_MLFQ     "test_mlfq"
+#define NOOP					"NOOP"
 
-char *child_argv[5][CNT_CHILD][3] = {
+char *child_argv[CNT_TEST][CNT_CHILD][3] = {
+	{ {NOOP, 0, 0},
+	  {NOOP, 0, 0},
+
+	}
   { {NAME_CHILD_MLFQ, "0", 0},
     {NAME_CHILD_MLFQ, "1", 0},
     {NAME_CHILD_STRIDE, "10", 0},
@@ -99,22 +105,22 @@ char *exceed_argv[CNT_CHILD][3] = {
   {NAME_CHILD_STRIDE, "30", 0},
   {NAME_CHILD_STRIDE, "15", 0},
   {NAME_CHILD_STRIDE, "6", 0},
-}
+};
 
 int
 main(int argc, char *argv[])
 {
   int pid;
   int i, j;
-  for (j = 0; j < 5; j++) {
-    for (i = 0; i < CNT_CHILD; i++) {
+  for (i = 0; i < CNT_TEST; i++) {
+    for (j = 0; j < CNT_CHILD; j++) {
       pid = fork();
       if (pid > 0) {
         // parent
         continue;
       } else if (pid == 0) {
         // child
-        exec(child_argv[j][i][0], child_argv[j][i]);
+        exec(child_argv[i][j][0], child_argv[i][j]);
         printf(1, "exec failed!!\n");
         exit();
       } else {
@@ -126,6 +132,7 @@ main(int argc, char *argv[])
     for (i = 0; i < CNT_CHILD; i++) {
       wait();
     }
+    printf("\n%d test done\n\n", i+1)
   }
   for (i = 0; i < CNT_EXCEED; i++) {
     pid = fork();
