f1a17d85aceed5dbeffaa2945f977b72c5fd07c0
prangia0ren@gmail.com <prangia0ren@gmail.com>
2018-05-22 00:29:11 +0900

    thread_fork.c

---
+++ proj_LWP/xv6-public/thread_fork.c
@@ -0,0 +1,48 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define NUM_THREAD 10
+
+void*
+forkthread(void *arg)
+{
+    int pid, waitreturn;
+    if ((pid = fork()) == -1) {
+        printf(1, "panic at fork in forktest\n");
+        exit();
+    } else if (pid == 0) {
+        printf(1, "child\n");
+        exit();
+    } else {
+        printf(1, "parent\n");
+        if ((waitreturn = wait()) != pid) {
+            // printf(1, "panic at wait in forktest\n");
+            printf(1, "expect %d but wait returned %d\n", pid, waitreturn);
+            // exit();
+        }
+    }
+    thread_exit(0);
+}
+
+int
+main(int argc, char *argv[])
+{
+    thread_t threads[NUM_THREAD];
+    int i;
+    void *retval;
+
+    for (i = 0; i < NUM_THREAD; i++) {
+        if (thread_create(&threads[i], forkthread, (void*)0) != 0) {
+            printf(1, "panic at thread_create\n");
+            return -1;
+        }
+    }
+    for (i = 0; i < NUM_THREAD; i++) {
+        if (thread_join(threads[i], &retval) != 0) {
+            printf(1, "panic at thread_join\n");
+            return -1;
+        }
+    }
+    exit();
+}
